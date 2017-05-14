6bcc2d531be99fb5b73fdc08fd729a0e754352e7
Konstantin Gredeskoul <kigster@gmail.com>
2017-05-14 16:24:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exokomodo CMake Template
+
+Originally sourced from [here](https://github.com/kigster/cmake-project-template)
---
+++ test/test.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Can create memory store", "[exo::MemoryStore]")
+{
+  REQUIRE(true);
+}
+
